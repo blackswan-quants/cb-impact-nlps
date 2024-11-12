4fb5d3b0f0fff5f4bb42c44dbe159c510f7d2c56
zanisimone <102178824+zanisimone@users.noreply.github.com>
2024-11-12 18:21:11 +0100

    update speakers splitting

--- main2.py
+++ main2.py
@@ -2,4 +2,6 @@ import pickle
 
 df = open ("./pickle_files/fedspeeches_preprocessed.pkl", "rb")
 df = pickle.load(df)
-print(df)
+
+unique_speakers = df["speaker"].unique().tolist()
+print(unique_speakers)
