fe8145593d8352e5b77dfefa1a99027ba800fa5e
zanisimone <102178824+zanisimone@users.noreply.github.com>
2024-11-18 19:55:28 +0100

    UPDATE: sentiment analysis fixed, pickle for 2024, 2023, 2022 added

--- helper_speech_analysis.py
+++ helper_speech_analysis.py
@@ -4,23 +4,10 @@ import pandas as pd
 import scipy
 import torch
 
-def load_finbert():
-    # Carichiamo il modello FinBERT e il tokenizer
-    model_name = "ProsusAI/finbert"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-    return tokenizer, model
-
-
-def build_pipeline(tokenizer, model):
-    # Creiamo il pipeline per l'analisi del sentiment
-    nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
-
-    return nlp
-
 
 def finbert_sentiment(text: str, tokenizer, model) -> tuple[float, float, float, str]:
+    
+
     with torch.no_grad():
         inputs = tokenizer(
             text, return_tensors="pt", padding=True, truncation=True, max_length=512
@@ -42,6 +29,6 @@ def finbert_sentiment(text: str, tokenizer, model) -> tuple[float, float, float,
         )
     
 # Funzione per analizzare il sentiment di ogni riga
-def analyze_sentiment(nlp ,text):
+def analyze_sentiment(text, nlp):
     result = nlp(text[:512])  # FinBERT ha un limite di 512 token
     return result[0]["label"], result[0]["score"]
---
+++ sentiment analysis.py
@@ -0,0 +1,45 @@
+import pickle
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+import pandas as pd
+import scipy
+import torch
+import helper_speech_analysis as sa
+import memory_handling as mh
+
+
+df = open ("./pickle_files/fedspeeches_preprocessed.pkl", "rb")
+df = pickle.load(df)
+df.drop(["title","link","text"], axis = 1, inplace=True)
+print(df)
+
+model_name = "ProsusAI/finbert"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+df = df[df["date"].dt.year == 2022]
+#df = df.head()
+print(df)
+
+
+# Notice that this is the raw text, no preprocessing
+df[["finbert_pos", "finbert_neg", "finbert_neu", "finbert_sentiment"]] = (
+    df["text_by_minute"].apply(lambda x: pd.Series(sa.finbert_sentiment(x, tokenizer, model)))
+)
+df["finbert_score"] = df["finbert_pos"] - df["finbert_neg"]
+
+
+# Applichiamo l'analisi del sentiment a ogni riga
+df["sentiment"], df["confidence"] = zip(*df["text_by_minute"].apply(lambda x : sa.analyze_sentiment(x, nlp)))
+
+# Salviamo i risultati in un nuovo file CSV
+df.to_csv("sentiment_2023.csv", index=False)
+pickle_helper = mh.PickleHelper(df)
+pickle_helper.pickle_dump('fedspeechees_sentiment_2022')
+
+print(df)
+
+
+
